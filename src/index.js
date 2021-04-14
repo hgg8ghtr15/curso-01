4f7ad4cb832a3d95fd34254b278b7f87d09d810e
hgg8ghtr15 <hgg8ghtr15>
2021-04-14 10:16:21 -0400

    Modulo 02 - Aula 01 a 06

--- src/index.js
+++ src/index.js
@@ -1,46 +1,93 @@
 let express = require('express')
 
+let { v4: uuidv4 } = require('uuid')
+
+//crio o App
 let app = express()
 
+//midware usado para receber um Json
 app.use(express.json())
 
-//Busca
-app.get("/",(request, response) => {
-    // return response.send("Bem vindo")
-    return response.json({message:"Bem vindo ao react"})
+//simula o banco de dados
+let customers = []
+
+//Middleware
+function vericicarCPF(request, response, next) {
+  //pega CPF
+  let {cpf} = request.headers
+
+  //procura o cpf
+  let customer = customers.find((customer) => customer.cpf === cpf)
+
+  //caso não achar devolve como erro 
+  if (!customer){
+    return response.status(400).json({ error: "Não foi encontrado CPF"})
+  }
+
+  //passo o customer para a function depois de next
+  request.customer = customer
+
+  //caso encontre devolve
+  return next()
+}
+
+
+/*
+* cpf - string
+* name - string
+* id - UUid
+* statement = estratos
+*/
+
+//criar acont
+app.post("/account", (request, response) => {
+  let { cpf, name } = request.body
+  let id = uuidv4()
+
+  //valida se cpf exeite
+  let cpf_existente = customers.some(
+    customer => customer.cpf === cpf
+  )
+  //Retorna error
+  if (cpf_existente) {
+    return response.status(400).json({ error: "Cpf já existe." })
+  }
+
+  //popula array caso não exista
+  customers.push(
+    {
+      cpf,
+      name,
+      id: uuidv4(),
+      statement: []
+    }
+  )
+  return response.status(201).send("Usuario Criado com sucesso!")
 })
 
-//Busca
-app.get("/cursos",(request, response) => {
-    let query = request.query
-    console.log(query)
-    return response.json(["Curso 1", "Curso 2", "Curso 3"])
-})
+/*
+app.get("/statement/:cpf", (request, response) => {
+  //desestrutura CPF
+  let { cpf } = request.params
 
-// post = inserir
-app.post("/cursos",(request, response) => {
-    let body = request.body
-    console.log(body)
-    return response.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"])
-})
+  //pesquisa e restorna 
+  let customer = customers.find(customer => customer.cpf === cpf)
+  // caso nao encontre restorna error
+  if (!customer) {
+    return response.status(400).json({ error: "Não foi encontrado CPF" })
+  }
 
-// put = alterar
-app.put("/cursos/:id",(request, response) => {
-    //Desestruturação
-    let {id} = request.params
-    console.log(id)
-    return response.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"])
+  return response.status(200).json(customer.statement)
 })
+*/
 
-// patch = alterar especifico
-app.patch("/cursos/:id",(request, response) => {
-    return response.json(["Curso 7", "Curso 2", "Curso 3", "Curso 4"])
+//para todas as rotas
+// app.use(vericicarCPF)
+
+//para uma rota especifica
+app.get("/statement", vericicarCPF, (request, response) => {
+  let {customer} = request
+  return response.status(200).json(customer.statement)
 })
-
-// delete = delata informações
-app.delete("/cursos/:id",(request, response) => {
-    return response.json(["Curso 7", "Curso 2", "Curso 4"])
-})
-
 
 app.listen(3333)
